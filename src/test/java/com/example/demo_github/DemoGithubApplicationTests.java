bb4912bcd2f469bd5e69a3dfcfb1b078fc52153a
SND <zalestev@gmail.com>
2026-03-13 22:54:33 +0100

    Test Failed workflow

--- src/test/java/com/example/demo_github/DemoGithubApplicationTests.java
+++ src/test/java/com/example/demo_github/DemoGithubApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo_github;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class DemoGithubApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals(20, 12+6, "Assertion volontairement fausse pour voir l'echec du workflow");
 	}
 
 }
